ce1c4cc8b44c23c3ca5bd1357c78b5d3da7355f4
OmVibhandik <omsvibhandik@gmail.com>
2023-12-27 07:58:35 +0530

    05: LoginForm - UnderStanding Context API Part1

---
+++ 09_miniContext/src/App.jsx
@@ -0,0 +1,20 @@
+import Profile from "./components/Profile"
+import Login from "./components/login"
+import UserContextProvider from "./context/UserContextProvider"
+import './App.css'
+
+
+
+function App() {
+
+
+  return (
+    <UserContextProvider>
+      <h1>Hello Cutiee</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
